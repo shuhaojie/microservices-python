6708d175d6cd07b3008cdce1a344315f339189d1
shuhaojie <shuhaojie@datagrand.cn>
2023-12-26 11:33:41 +0800

    修改auth服务

--- auth/tests/login.py
+++ auth/tests/login.py
@@ -1,7 +1,7 @@
 import requests
 
 res = requests.post(
-    url='http://127.0.0.1',
+    url='http://127.0.0.1:5000/login',
     data={"username": "georgio@email.com", "password": "Admin123"}
 )
 print(res.text)
